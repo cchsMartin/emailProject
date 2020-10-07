003fa6afea16143d46f573efc8d2e5da9599a8e8
katehovey <71032340+katehovey@users.noreply.github.com>
2020-10-07 12:49:37 -0400

    Add files via upload

---
+++ emailProject.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class emailProject {
+
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in); 
+	    
+	    System.out.println("Please enter your first name");
+        String fn = input.nextLine(); 
+        
+	    System.out.println("Please enter your middle name");
+        String mn = input.nextLine(); 
+	    
+	    System.out.println("Please enter your last name");
+        String ln = input.nextLine(); 
+        
+	    System.out.println("Please enter your birthday in the format (mmddyyyy)");
+        String bd = input.nextLine(); 
+        
+        ln = ln.toLowerCase();
+        String fi = fn.substring(0,1);
+        fi = fi.toUpperCase();
+        String mi = mn.substring(0,1);
+        mi = mi.toUpperCase();
+        
+        String email = (ln + fi + mi + "@sharkmail.com");
+        String password = bd;
+        
+        System.out.println("Your email is: " + email);
+        System.out.println("Your password is: " + password);
+        System.out.println("Your current password is weak. Please change immediately.");
+	}
+
+}
